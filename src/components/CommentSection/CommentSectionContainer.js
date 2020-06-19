2a69ecc7ba52415f7c0e45ffabb0df2661f0b0a8
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-06-19 13:51:15 -0500

    Working on routing props

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -3,13 +3,16 @@ import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
+import dummyData from "../../dummy-data";
 
 const CommentSection = props => {
-  // Add state for the comments
+ let [comment, commentState] = useState(0);
 
   return (
     <div>
-      {/* map through the comments data and return the Comment component */}
+      {dummyData.map((comment) => {
+
+      })}
       <CommentInput />
     </div>
   );
